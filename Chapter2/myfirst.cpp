3db0e8c44b408e394231f253ef40c4bbad1d6b13
Tianci Gao <17771618951@163.com>
2024-03-12 17:07:26 +0800

    第一次提交

---
+++ Chapter2/myfirst.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main(){
+    // using namespace std;
+    std::cout << "Come up and C++ me some time.";//使用std命名空间里面的cout函数
+    std::cout << std::endl;//使用std命名空间里面的endl函数
+    std::cout << "You won't regret it!" << std::endl;
+    return 0;
+}
--- Project1/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "max.h"//引用自己的头文件
-int main(){
-    int a = 10;
-    int b = 30;
-    int c = Max(a,b);
-    printf("%d\n",c);
-    return 0;
-}
